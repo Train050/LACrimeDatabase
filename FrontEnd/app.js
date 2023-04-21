edbdfc75622d3aa7b6a1fa8c845832a0c993f5c6
Train050 <keegan.sepiol@ufl.edu>
2023-04-21 02:39:02 -0400

    FV Graph

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -225,30 +225,14 @@ document.addEventListener("DOMContentLoaded", (event) =>{
     FV = document.getElementById("FVChart");
 
     if(FV){
-        const labels = ['January','February','March','April','May','June','July'];
+        const labels = ['Gen Z (0-26)', 'Millennial (27-42)', 'Gen X (43-58)', 'Boomers II (59-68), Boomers I (69+)'];
         const data = {
             labels: labels,
             datasets: [{
-                label: 'My First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 205, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(201, 203, 207, 0.2)'
-                ],
+                data: [65, 59, 80, 81, 32],
+                backgroundColor: [colorScheme[0], colorScheme[1], colorScheme[2], colorScheme[3], colorScheme[4]],
                 borderColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 159, 64)',
-                    'rgb(255, 205, 86)',
-                    'rgb(75, 192, 192)',
-                    'rgb(54, 162, 235)',
-                    'rgb(153, 102, 255)',
-                    'rgb(201, 203, 207)'
-                ],
+                    colorScheme[0], colorScheme[1], colorScheme[2], colorScheme[3], colorScheme[4]],
                 borderWidth: 1
             }]
         };
@@ -321,11 +305,7 @@ document.addEventListener("DOMContentLoaded", (event) =>{
         sliderGC.addEventListener("change", (event) =>{
             updateGCChart();
         })
-
-
     }
-
-
 
     TBS = document.getElementById("TBSChart");
     
@@ -335,7 +315,7 @@ document.addEventListener("DOMContentLoaded", (event) =>{
         const changeYear = document.getElementById('TheftBySeason_Input');
 
         changeYear.addEventListener('change', (event) => {
-            theftYear = event.target.value;
+            theftYear = changeYear.value;
         });
 
         let springData = [];
